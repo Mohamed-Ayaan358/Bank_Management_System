973c53778ef074c77d4b4e647c1d10ada3a5887f
Mohamed Ayaan <72858215+Mohamed-Ayaan358@users.noreply.github.com>
2021-06-11 22:26:57 +0530

    Update header.h

--- header.h
+++ header.h
@@ -17,9 +17,6 @@ struct
     struct date dob;
     struct date deposit;
     struct date withdraw;
-    
-    char upinp[30];
-    char new_phone[11];
 
 } add, update, check, rem, upi, transact; /*Allows you to refer to the struct using different access points*/
 
